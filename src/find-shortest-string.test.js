3450a83411de164d606005aaad94c61c79ebb99f
Zahra <bghzahra@outlook.com>
2022-02-23 22:22:19 +0000

    formatted and linted

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,10 +1,10 @@
 const findShortestString = require("./shortest-string");
 
 test("findShortestString finds the shortest string in an array", () => {
-  expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("a");
-  expect(findLongestString(["why", "hello", "to", "you"])).toBe("to");
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
 test("findShortestString returns the earlier string in cases of joint longest strings", () => {
-  expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
 });
